6b316e10cab9ac20f5697a9a62de551c42f53ed2
纳西妲 · Nahida <yanwenz551@gmail.com>
2024-01-31 22:52:20 +0800

    change_opts(lualine): Override the default configuration

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -1,11 +1,13 @@
 return {
   "nvim-lualine/lualine.nvim",
   optional = true,
-  opts = {
-    options = require(-star.path.Plugins.Lualine .. ".options"),
-    -- opts.tabline = require(-star.path.Plugins.Lualine.. ".tabline")
-    -- opts.winbar = require(-star.path.Plugins.Lualine.. ".winbar")
-    sections = require(-star.path.Plugins.Lualine .. ".sections"),
-    extensions = require(-star.path.Plugins.Lualine .. ".extensions"),
-  },
+  opts = function()
+    return {
+      options = require(-star.path.Plugins.Lualine .. ".options"),
+      -- opts.tabline = require(-star.path.Plugins.Lualine.. ".tabline")
+      -- opts.winbar = require(-star.path.Plugins.Lualine.. ".winbar")
+      sections = require(-star.path.Plugins.Lualine .. ".sections"),
+      extensions = require(-star.path.Plugins.Lualine .. ".extensions"),
+    }
+  end,
 }
