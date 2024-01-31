43f64d7b59259ed3c2e2c0190864a1fb26dcaa44
纳西妲 · Nahida <yanwenz551@gmail.com>
2024-01-31 22:37:08 +0800

    refactor(indent): Change the way you do it

--- lua/config/autocmds/indent.lua
+++ lua/config/autocmds/indent.lua
@@ -1,9 +1,9 @@
 return function()
   vim.api.nvim_create_autocmd("FileType", {
     pattern = { "rust", "hyprlang" },
-    callback = function()
-      vim.opt_local.tabstop = 2
-      vim.opt_local.shiftwidth = 2
+    callback = function(event)
+      vim.bo[event.buf].tabstop = 4
+      vim.bo[event.buf].shiftwidth = 4
     end,
   })
 end
