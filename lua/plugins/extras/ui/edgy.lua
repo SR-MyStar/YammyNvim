b8fe21f35d3f25e1b8e3e78af0e930b81632d573
纳西妲 · Nahida <yanwenz551@gmail.com>
2024-02-02 03:43:25 +0800

    fix(edgy): Terminal interface error

--- lua/plugins/extras/ui/edgy.lua
+++ lua/plugins/extras/ui/edgy.lua
@@ -1,14 +1,24 @@
 return {
-  "folke/edgy.nvim",
-  opts = function(_, opts)
-    opts.top = vim.list_extend(opts.top or {}, {
-      {
-        ft = "terminal",
-        size = { height = 0.3 },
-        filter = function(buf, win)
-          return vim.api.nvim_win_get_config(win).relative == ""
-        end,
+  {
+    "folke/edgy.nvim",
+    opts = {
+      animate = {
+        enabled = false,
       },
-    })
-  end,
+    },
+  },
+  {
+    "folke/edgy.nvim",
+    opts = function(_, opts)
+      opts.top = vim.list_extend(opts.top or {}, {
+        {
+          ft = "terminal",
+          size = { height = 0.3 },
+          filter = function(buf, win)
+            return vim.api.nvim_win_get_config(win).relative == ""
+          end,
+        },
+      })
+    end,
+  },
 }
