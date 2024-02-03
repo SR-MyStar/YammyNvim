237d5629e2df4b1faa71d33dc095de489cc64a03
纳西妲 · Nahida <yanwenz551@gmail.com>
2024-02-03 23:09:47 +0800

    keymap(goto): Add keymap `<M-b>` link to `%` key

--- lua/config/keymaps/goto.lua
+++ lua/config/keymaps/goto.lua
@@ -2,6 +2,7 @@ return {
   keys = {
     ["<M-h>"] = { "^", "Goto the beginning of the line" },
     ["<M-l>"] = { "$", "Goto the end of the line" },
+    ["<M-b>"] = { "%", "Cycle forward through results" },
   },
   opts = {
     mode = { "n", "o", "x" },
