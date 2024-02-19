74282d2d4e11f25e68ee6604db9d802b8385bec8
纳西妲 · Nahida <yanwenz551@gmail.com>
2024-02-19 09:24:55 +0800

    feat(Comment.nvim): in opts `lazy = true`

--- lua/plugins/coding/comment.lua
+++ lua/plugins/coding/comment.lua
@@ -2,7 +2,7 @@ return {
   { "echasnovski/mini.comment", enabled = false },
   {
     "numToStr/Comment.nvim",
-    lazy = false,
+    lazy = true,
     opts = {
       pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
     },
