bd339251eab008583e2bb565cd9eef99dfcbcbe5
纳西妲 · Nahida <yanwenz551@gmail.com>
2024-02-19 08:30:06 +0800

    feat!: add a plugin and remove a plugin
    Add: `Comment.nvim`
    Remove: `mini.comment`

---
+++ lua/plugins/coding/comment.lua
@@ -0,0 +1,18 @@
+return {
+  { "echasnovski/mini.comment", enabled = false },
+  {
+    "numToStr/Comment.nvim",
+    lazy = false,
+    opts = {},
+    keys = {
+      "gcc",
+      "gbc",
+
+      "gcO",
+      "gco",
+      "gcA",
+      { "gc", mode = "x" },
+      { "gb", mode = "x" },
+    },
+  },
+}
