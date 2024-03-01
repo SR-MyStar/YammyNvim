2284a3b04607df3117fef1ed8f7f7426ec7044fa
纳西妲 · Nahida <yanwenz551@gmail.com>
2024-03-02 03:08:56 +0800

    feat(indent.lua): add `kdl`

--- lua/config/autocmds/indent.lua
+++ lua/config/autocmds/indent.lua
@@ -1,6 +1,6 @@
 return function()
   vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "rust", "hyprlang" },
+    pattern = { "rust", "hyprlang", "kdl" },
     callback = function(event)
       vim.bo[event.buf].tabstop = 4
       vim.bo[event.buf].shiftwidth = 4
