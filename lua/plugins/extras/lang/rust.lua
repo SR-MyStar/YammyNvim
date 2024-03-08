0e8838623174994b258d88da0b2598394139096f
纳西妲 · Nahida <yanwenz551@gmail.com>
2024-03-08 22:51:32 +0800

    feat(todo-comments.nvim): add todo highlighting for rust placeholders

--- lua/plugins/extras/lang/rust.lua
+++ lua/plugins/extras/lang/rust.lua
@@ -1,9 +1,26 @@
 return {
-  "simrat39/rust-tools.nvim",
-  lazy = true,
-  opts = {
-    tools = {
-      inlay_hints = { auto = false },
+  {
+    "simrat39/rust-tools.nvim",
+    lazy = true,
+    opts = {
+      tools = {
+        inlay_hints = { auto = false },
+      },
+    },
+  },
+  {
+    "folke/todo-comments.nvim",
+    opts = {
+      keywords = {
+        TODO = {
+          alt = { "todo", "unimplemented" },
+        },
+      },
+      highlight = {
+        pattern = { [[.*<(KEYWORDS)\s*:]], [[.*<(KEYWORDS)\s*!\(]] },
+        comments_only = false,
+      },
+      search = { pattern = [=[\b(KEYWORDS)(:|!\()]=] },
     },
   },
 }
